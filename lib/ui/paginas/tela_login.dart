c2c80608bf09cc7353807d75275a8091330ac6cc
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-05-26 10:45:48 -0300

    style: adiciona layout responsivo a tela de login

--- lib/ui/paginas/tela_login.dart
+++ lib/ui/paginas/tela_login.dart
@@ -220,170 +220,172 @@ class _TelaLoginState extends State<TelaLogin> {
     return Padding(
       padding: EdgeInsets.all(50),
       child: Row(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            width: 300.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
+          width: 300.0,  
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
               color: Color.fromRGBO(195, 0, 10, 1.0),
             ),
           ),
-          Column(
-            children: [
-              Text(
-                "Bem-vindo!",
-                style: TextStyle(
-                  decorationColor: Color.fromRGBO(64, 64, 64, 1.0),
-                  color: Color.fromRGBO(64, 64, 64, 1.0),
-                  fontSize: 24,
-                  fontFamily: "IstokWeb",
-                  fontWeight: FontWeight.bold,
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "Bem-vindo!",
+                  style: TextStyle(
+                    decorationColor: Color.fromRGBO(64, 64, 64, 1.0),
+                    color: Color.fromRGBO(64, 64, 64, 1.0),
+                    fontSize: 24,
+                    fontFamily: "IstokWeb",
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Complete seus dados para continuar",
-                style: TextStyle(
-                  decorationColor: Color.fromRGBO(64, 64, 64, 1.0),
-                  decoration: TextDecoration.underline,
-                  color: Color.fromRGBO(64, 64, 64, 0.8),
-                  fontSize: 12,
-                  fontFamily: "IstokWeb",
+                Text(
+                  "Complete seus dados para continuar",
+                  style: TextStyle(
+                    decorationColor: Color.fromRGBO(64, 64, 64, 1.0),
+                    decoration: TextDecoration.underline,
+                    color: Color.fromRGBO(64, 64, 64, 0.8),
+                    fontSize: 12,
+                    fontFamily: "IstokWeb",
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      spacing: 10.0,
-                      children: [
-                        TextField(
-                          controller: nomeController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Column(
+                          spacing: 10.0,
+                          children: [
+                            TextField(
+                              controller: nomeController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Color.fromRGBO(234, 234, 234, 1.0),
+                                hint: Text(
+                                  "Nome Completo",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(64, 64, 64, 1.0),
+                                  ),
+                                ),
                               ),
+                              onChanged: (valor) {
+                                setState(() {});
+                              },
                             ),
-                            filled: true,
-                            fillColor: Color.fromRGBO(234, 234, 234, 1.0),
-                            hint: Text(
-                              "Nome Completo",
-                              style: TextStyle(
-                                color: Color.fromRGBO(64, 64, 64, 1.0),
+                            TextField(
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Color.fromRGBO(234, 234, 234, 1.0),
+                                hint: Text(
+                                  "E-mail institucional",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(64, 64, 64, 1.0),
+                                  ),
+                                ),
                               ),
+                              onChanged: (valor) {
+                                setState(() {});
+                              },
                             ),
-                          ),
-                          onChanged: (valor) {
-                            setState(() {});
-                          },
-                        ),
-                        TextField(
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
+                            TextField(
+                              controller: senhaController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Color.fromRGBO(234, 234, 234, 1.0),
+                                hint: Text(
+                                  "Senha institucional",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(64, 64, 64, 1.0),
+                                  ),
+                                ),
                               ),
+                              onChanged: (valor) {
+                                setState(() {});
+                              },
                             ),
-                            filled: true,
-                            fillColor: Color.fromRGBO(234, 234, 234, 1.0),
-                            hint: Text(
-                              "E-mail institucional",
-                              style: TextStyle(
-                                color: Color.fromRGBO(64, 64, 64, 1.0),
+                            TextField(
+                              controller: repetirSenhaController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Color.fromRGBO(234, 234, 234, 1.0),
+                                hint: Text(
+                                  "Repita a senha",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(64, 64, 64, 1.0),
+                                  ),
+                                ),
                               ),
+                              onChanged: (valor) {
+                                setState(() {});
+                              },
                             ),
-                          ),
-                          onChanged: (valor) {
-                            setState(() {});
-                          },
-                        ),
-                        TextField(
-                          controller: senhaController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),
-                            ),
-                            filled: true,
-                            fillColor: Color.fromRGBO(234, 234, 234, 1.0),
-                            hint: Text(
-                              "Senha institucional",
-                              style: TextStyle(
-                                color: Color.fromRGBO(64, 64, 64, 1.0),
-                              ),
-                            ),
-                          ),
-                          onChanged: (valor) {
-                            setState(() {});
-                          },
-                        ),
-                        TextField(
-                          controller: repetirSenhaController,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10.0),
-                              ),
-                            ),
-                            filled: true,
-                            fillColor: Color.fromRGBO(234, 234, 234, 1.0),
-                            hint: Text(
-                              "Repita a senha",
-                              style: TextStyle(
-                                color: Color.fromRGBO(64, 64, 64, 1.0),
-                              ),
-                            ),
-                          ),
-                          onChanged: (valor) {
-                            setState(() {});
-                          },
-                        ),
-                      ],
-                    ),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        fixedSize: WidgetStatePropertyAll<Size>(Size(
-                          250.0,
-                          40.0)
-                        ),
-                        backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                          (Set<WidgetState> states) {
-                            return Color.fromRGBO(195, 0, 10, 1.0);
-                          }
+                          ],
                         ),
                       ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) => const WidgetTree(),
-                          )
-                        );
-                      },
-                      child: Text(
-                        "Continuar",
-                        style: TextStyle(
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          fixedSize: WidgetStatePropertyAll<Size>(Size(
+                            250.0,
+                            40.0)
+                          ),
+                          backgroundColor: WidgetStateProperty.resolveWith<Color?>(
+                            (Set<WidgetState> states) {
+                              return Color.fromRGBO(195, 0, 10, 1.0);
+                            }
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => const WidgetTree(),
+                            )
+                          );
+                        },
+                        child: Text(
+                          "Continuar",
+                          style: TextStyle(
                             color: Color.fromRGBO(255, 255, 255, 1.0),
                           ),
                         )
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
